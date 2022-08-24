d7b882707f6ca7fe59d725c6c7a14e3666325d5f
Anatoliy Avramenko <mojloc@yandex.ru>
2022-08-24 20:32:25 +0700

    Removed unnecessary comments

--- src/main/java/net/mojloc/sortthis/SortThis.java
+++ src/main/java/net/mojloc/sortthis/SortThis.java
@@ -6,16 +6,6 @@ import java.util.List;
 public class SortThis {
 
     public static void main(String[] args) {
-        //TODO remove args from method body after debugging
-//        args = new String[]{"-i", "-a", "src\\main\\resources\\result.txt", "src\\main\\resources\\int1.txt"
-//                           , "src\\main\\resources\\int2.txt", "src\\main\\resources\\int3.txt", "src\\main\\resources\\int4.txt"};
-
-//        args = new String[]{"-i", "-d", "src\\main\\resources\\result1.txt", "src\\main\\resources\\int5.txt"
-//                , "src\\main\\resources\\int6.txt", "src\\main\\resources\\int7.txt", "src\\main\\resources\\int8.txt"};
-
-        args = new String[]{"-s", "-a", "src\\main\\resources\\result2.txt", "src\\main\\resources\\str1.txt"
-                , "src\\main\\resources\\str2.txt", "src\\main\\resources\\str3.txt", "src\\main\\resources\\str4.txt"};
-
         System.out.println(Messages.START_MESSAGE.getMessage());
 
         ArgumentParser argumentParser = new ArgumentParser();
